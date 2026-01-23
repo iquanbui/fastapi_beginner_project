7d8c2add5b817a657a8710b6486a4f021a958e03
*W60mPsbrlU <bui.van.quan@jam-tech.jp>
2026-01-23 18:57:49 +0900

    clearning

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.api.endpoints import projects
 from app.core import config
 
 app = FastAPI(title=config.PROJECT_NAME, version=config.PROJECT_VERSION)
 
 # Include routers
-app.include_router(projects.router, prefix="/api/v1/projects", tags=["projects"])
+# app.include_router(router, prefix="/api/v1/resource", tags=["resource"])
+
 
 @app.get("/")
 def read_root():
